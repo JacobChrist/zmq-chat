d7671433840c28668058b521eb42889135eb75e2
Jacob Christ <jacob@pontech.com>
2012-04-30 09:52:04 -0700

    ZeroMQ vb.net VS2010 chat example initial check-in

---
+++ zmq-chat/frmChat.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmChat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ServerButton = New System.Windows.Forms.Button()
+        Me.ClientButton = New System.Windows.Forms.Button()
+        Me.ServerTextBox = New System.Windows.Forms.TextBox()
+        Me.ClientTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ServerButton
+        '
+        Me.ServerButton.Location = New System.Drawing.Point(12, 12)
+        Me.ServerButton.Name = "ServerButton"
+        Me.ServerButton.Size = New System.Drawing.Size(86, 23)
+        Me.ServerButton.TabIndex = 1
+        Me.ServerButton.Text = "Start Server"
+        Me.ServerButton.UseVisualStyleBackColor = True
+        '
+        'ClientButton
+        '
+        Me.ClientButton.Location = New System.Drawing.Point(160, 12)
+        Me.ClientButton.Name = "ClientButton"
+        Me.ClientButton.Size = New System.Drawing.Size(86, 23)
+        Me.ClientButton.TabIndex = 2
+        Me.ClientButton.Text = "Start Client"
+        Me.ClientButton.UseVisualStyleBackColor = True
+        '
+        'ServerTextBox
+        '
+        Me.ServerTextBox.Location = New System.Drawing.Point(12, 41)
+        Me.ServerTextBox.Multiline = True
+        Me.ServerTextBox.Name = "ServerTextBox"
+        Me.ServerTextBox.Size = New System.Drawing.Size(142, 220)
+        Me.ServerTextBox.TabIndex = 3
+        '
+        'ClientTextBox
+        '
+        Me.ClientTextBox.Location = New System.Drawing.Point(160, 41)
+        Me.ClientTextBox.Multiline = True
+        Me.ClientTextBox.Name = "ClientTextBox"
+        Me.ClientTextBox.Size = New System.Drawing.Size(142, 220)
+        Me.ClientTextBox.TabIndex = 4
+        '
+        'frmChat
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 273)
+        Me.Controls.Add(Me.ClientTextBox)
+        Me.Controls.Add(Me.ServerTextBox)
+        Me.Controls.Add(Me.ClientButton)
+        Me.Controls.Add(Me.ServerButton)
+        Me.Name = "frmChat"
+        Me.Text = "frmChat ZeroMQ"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ServerButton As System.Windows.Forms.Button
+    Friend WithEvents ClientButton As System.Windows.Forms.Button
+    Friend WithEvents ServerTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ClientTextBox As System.Windows.Forms.TextBox
+
+End Class
---
+++ zmq-chat/frmChat.vb
@@ -0,0 +1,94 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Threading
+Imports ZeroMQ
+
+Public Class frmChat
+    Dim server As New Thread(AddressOf ServerThread)
+
+    Delegate Sub TextBoxAppendText(ByVal output As String)
+
+    Public Sub ClientTextBoxAppendText_Callback(ByVal output As String)
+        ClientTextBox.AppendText(output + vbCrLf)
+    End Sub
+
+    Public Sub ServerTextBoxAppendText_Callback(ByVal output As String)
+        ServerTextBox.AppendText(output + vbCrLf)
+    End Sub
+
+    Private Sub ServerButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ServerButton.Click
+        If server.IsAlive Then
+            MsgBox("Server is running")
+        Else
+            server.Start()
+        End If
+    End Sub
+
+    Private Sub ClientButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClientButton.Click
+        Dim client As New Thread(AddressOf ClientThread)
+        client.Start()
+        'ClientThread() // for some reason this caused the server to hang
+    End Sub
+
+    Private Sub ClientThread()
+        Dim context As ZmqContext = ZmqContext.Create()
+        Dim socket As ZmqSocket = context.CreateSocket(SocketType.REQ)
+        Using socket
+            socket.Connect("tcp://localhost:8989")
+
+            socket.SendFrame(New Frame(Encoding.UTF8.GetBytes("Hello")))
+
+            Dim buffer(100) As Byte
+            Dim size As Integer = socket.Receive(buffer)
+
+            Dim stream As New MemoryStream(buffer, 0, size)
+            Using stream
+                If ClientTextBox.InvokeRequired Then
+                    Dim d As New TextBoxAppendText(AddressOf ClientTextBoxAppendText_Callback)
+                    Me.Invoke(d, New Object() {Encoding.UTF8.GetString(stream.ToArray())})
+                Else
+                    ClientTextBox.Text = Encoding.UTF8.GetString(stream.ToArray())
+                End If
+
+                Console.WriteLine(Encoding.UTF8.GetString(stream.ToArray()))
+            End Using
+        End Using
+    End Sub
+
+    Private Sub ServerThread()
+        Try
+            Dim context As ZmqContext = ZmqContext.Create()
+            Dim socket As ZmqSocket = context.CreateSocket(SocketType.REP)
+
+            Using socket
+                socket.Bind("tcp://*:8989")
+
+                While (1)
+                    Dim request As Frame = socket.ReceiveFrame(New TimeSpan(10000000))
+
+                    If request.BufferSize > 0 Then
+                        If ServerTextBox.InvokeRequired Then
+                            Dim d As New TextBoxAppendText(AddressOf ServerTextBoxAppendText_Callback)
+                            Me.Invoke(d, New Object() {Encoding.UTF8.GetString(request)})
+                        Else
+                            ServerTextBox.Text = Encoding.UTF8.GetString(request)
+                        End If
+
+                        Console.WriteLine(Encoding.UTF8.GetString(request))
+                        socket.SendFrame(New Frame(Encoding.UTF8.GetBytes("World")))
+                    End If
+                End While
+            End Using
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If server.IsAlive Then
+            server.Abort()
+        End If
+    End Sub
+
+End Class
