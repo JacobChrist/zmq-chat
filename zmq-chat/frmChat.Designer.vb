35268271d3cef0f69c25890be35be8ab2fab6859
Jacob Christ <jacob@pontech.com>
2012-05-01 02:18:33 -0700

    Added ability to start and stop server. Added textboxes to configure connect strings. Added textboxes for messages to send.

--- zmq-chat/frmChat.Designer.vb
+++ zmq-chat/frmChat.Designer.vb
@@ -26,13 +26,19 @@ Partial Class frmChat
         Me.ClientButton = New System.Windows.Forms.Button()
         Me.ServerTextBox = New System.Windows.Forms.TextBox()
         Me.ClientTextBox = New System.Windows.Forms.TextBox()
+        Me.ServerConnectString = New System.Windows.Forms.TextBox()
+        Me.ClientConnectString = New System.Windows.Forms.TextBox()
+        Me.ServerStringTextBox = New System.Windows.Forms.TextBox()
+        Me.ClientStringTextBox = New System.Windows.Forms.TextBox()
+        Me.GetMyIpButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ServerButton
         '
         Me.ServerButton.Location = New System.Drawing.Point(12, 12)
         Me.ServerButton.Name = "ServerButton"
-        Me.ServerButton.Size = New System.Drawing.Size(86, 23)
+        Me.ServerButton.Size = New System.Drawing.Size(142, 23)
         Me.ServerButton.TabIndex = 1
         Me.ServerButton.Text = "Start Server"
         Me.ServerButton.UseVisualStyleBackColor = True
@@ -41,32 +47,88 @@ Partial Class frmChat
         '
         Me.ClientButton.Location = New System.Drawing.Point(160, 12)
         Me.ClientButton.Name = "ClientButton"
-        Me.ClientButton.Size = New System.Drawing.Size(86, 23)
+        Me.ClientButton.Size = New System.Drawing.Size(142, 23)
         Me.ClientButton.TabIndex = 2
         Me.ClientButton.Text = "Start Client"
         Me.ClientButton.UseVisualStyleBackColor = True
         '
         'ServerTextBox
         '
-        Me.ServerTextBox.Location = New System.Drawing.Point(12, 41)
+        Me.ServerTextBox.Location = New System.Drawing.Point(12, 93)
         Me.ServerTextBox.Multiline = True
         Me.ServerTextBox.Name = "ServerTextBox"
-        Me.ServerTextBox.Size = New System.Drawing.Size(142, 220)
+        Me.ServerTextBox.Size = New System.Drawing.Size(142, 168)
         Me.ServerTextBox.TabIndex = 3
         '
         'ClientTextBox
         '
-        Me.ClientTextBox.Location = New System.Drawing.Point(160, 41)
+        Me.ClientTextBox.Location = New System.Drawing.Point(160, 93)
         Me.ClientTextBox.Multiline = True
         Me.ClientTextBox.Name = "ClientTextBox"
-        Me.ClientTextBox.Size = New System.Drawing.Size(142, 220)
+        Me.ClientTextBox.Size = New System.Drawing.Size(142, 168)
         Me.ClientTextBox.TabIndex = 4
+        '
+        'ServerConnectString
+        '
+        Me.ServerConnectString.Location = New System.Drawing.Point(12, 41)
+        Me.ServerConnectString.Name = "ServerConnectString"
+        Me.ServerConnectString.Size = New System.Drawing.Size(142, 20)
+        Me.ServerConnectString.TabIndex = 5
+        Me.ServerConnectString.Text = "tcp://*:8989"
+        '
+        'ClientConnectString
+        '
+        Me.ClientConnectString.Location = New System.Drawing.Point(160, 41)
+        Me.ClientConnectString.Name = "ClientConnectString"
+        Me.ClientConnectString.Size = New System.Drawing.Size(142, 20)
+        Me.ClientConnectString.TabIndex = 6
+        Me.ClientConnectString.Text = "tcp://localhost:8989"
+        '
+        'ServerStringTextBox
+        '
+        Me.ServerStringTextBox.Location = New System.Drawing.Point(12, 67)
+        Me.ServerStringTextBox.Name = "ServerStringTextBox"
+        Me.ServerStringTextBox.Size = New System.Drawing.Size(142, 20)
+        Me.ServerStringTextBox.TabIndex = 7
+        Me.ServerStringTextBox.Text = "World"
+        '
+        'ClientStringTextBox
+        '
+        Me.ClientStringTextBox.Location = New System.Drawing.Point(160, 67)
+        Me.ClientStringTextBox.Name = "ClientStringTextBox"
+        Me.ClientStringTextBox.Size = New System.Drawing.Size(142, 20)
+        Me.ClientStringTextBox.TabIndex = 8
+        Me.ClientStringTextBox.Text = "Hello"
+        '
+        'GetMyIpButton
+        '
+        Me.GetMyIpButton.Location = New System.Drawing.Point(12, 267)
+        Me.GetMyIpButton.Name = "GetMyIpButton"
+        Me.GetMyIpButton.Size = New System.Drawing.Size(142, 23)
+        Me.GetMyIpButton.TabIndex = 9
+        Me.GetMyIpButton.Text = "Get My IP"
+        Me.GetMyIpButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(160, 267)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(142, 23)
+        Me.ClearButton.TabIndex = 10
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
         '
         'frmChat
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(314, 273)
+        Me.ClientSize = New System.Drawing.Size(314, 297)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.GetMyIpButton)
+        Me.Controls.Add(Me.ClientStringTextBox)
+        Me.Controls.Add(Me.ServerStringTextBox)
+        Me.Controls.Add(Me.ClientConnectString)
+        Me.Controls.Add(Me.ServerConnectString)
         Me.Controls.Add(Me.ClientTextBox)
         Me.Controls.Add(Me.ServerTextBox)
         Me.Controls.Add(Me.ClientButton)
@@ -81,5 +143,11 @@ Partial Class frmChat
     Friend WithEvents ClientButton As System.Windows.Forms.Button
     Friend WithEvents ServerTextBox As System.Windows.Forms.TextBox
     Friend WithEvents ClientTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ServerConnectString As System.Windows.Forms.TextBox
+    Friend WithEvents ClientConnectString As System.Windows.Forms.TextBox
+    Friend WithEvents ServerStringTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ClientStringTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents GetMyIpButton As System.Windows.Forms.Button
+    Friend WithEvents ClearButton As System.Windows.Forms.Button
 
 End Class
--- zmq-chat/frmChat.vb
+++ zmq-chat/frmChat.vb
@@ -5,7 +5,7 @@ Imports System.Threading
 Imports ZeroMQ
 
 Public Class frmChat
-    Dim server As New Thread(AddressOf ServerThread)
+    Dim server As Thread
 
     Delegate Sub TextBoxAppendText(ByVal output As String)
 
@@ -18,10 +18,16 @@ Public Class frmChat
     End Sub
 
     Private Sub ServerButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ServerButton.Click
+        If server Is Nothing Then
+            server = New Thread(AddressOf ServerThread)
+        End If
         If server.IsAlive Then
-            MsgBox("Server is running")
+            server.Abort()
+            ServerButton.Text = "Start Server"
         Else
+            server = New Thread(AddressOf ServerThread)
             server.Start()
+            ServerButton.Text = "Stop Server"
         End If
     End Sub
 
@@ -35,20 +41,21 @@ Public Class frmChat
         Dim context As ZmqContext = ZmqContext.Create()
         Dim socket As ZmqSocket = context.CreateSocket(SocketType.REQ)
         Using socket
-            socket.Connect("tcp://localhost:8989")
+            socket.Connect(ClientConnectString.Text)
 
-            socket.SendFrame(New Frame(Encoding.UTF8.GetBytes("Hello")))
+            socket.SendFrame(New Frame(Encoding.UTF8.GetBytes(ClientStringTextBox.Text)))
 
             Dim buffer(100) As Byte
             Dim size As Integer = socket.Receive(buffer)
 
             Dim stream As New MemoryStream(buffer, 0, size)
+            Dim DisplayString As String = ClientStringTextBox.Text + "->" + Encoding.UTF8.GetString(stream.ToArray())
             Using stream
                 If ClientTextBox.InvokeRequired Then
                     Dim d As New TextBoxAppendText(AddressOf ClientTextBoxAppendText_Callback)
-                    Me.Invoke(d, New Object() {Encoding.UTF8.GetString(stream.ToArray())})
+                    Me.Invoke(d, New Object() {DisplayString})
                 Else
-                    ClientTextBox.Text = Encoding.UTF8.GetString(stream.ToArray())
+                    ClientTextBox.Text = DisplayString
                 End If
 
                 Console.WriteLine(Encoding.UTF8.GetString(stream.ToArray()))
@@ -62,21 +69,23 @@ Public Class frmChat
             Dim socket As ZmqSocket = context.CreateSocket(SocketType.REP)
 
             Using socket
-                socket.Bind("tcp://*:8989")
+                socket.Bind(ServerConnectString.Text)
 
                 While (1)
                     Dim request As Frame = socket.ReceiveFrame(New TimeSpan(10000000))
 
+                    Dim DisplayString As String = Encoding.UTF8.GetString(request) + "<-" + ServerStringTextBox.Text
+
                     If request.BufferSize > 0 Then
                         If ServerTextBox.InvokeRequired Then
                             Dim d As New TextBoxAppendText(AddressOf ServerTextBoxAppendText_Callback)
-                            Me.Invoke(d, New Object() {Encoding.UTF8.GetString(request)})
+                            Me.Invoke(d, New Object() {DisplayString})
                         Else
-                            ServerTextBox.Text = Encoding.UTF8.GetString(request)
+                            ServerTextBox.Text = DisplayString
                         End If
 
                         Console.WriteLine(Encoding.UTF8.GetString(request))
-                        socket.SendFrame(New Frame(Encoding.UTF8.GetBytes("World")))
+                        socket.SendFrame(New Frame(Encoding.UTF8.GetBytes(ServerStringTextBox.Text)))
                     End If
                 End While
             End Using
@@ -91,4 +100,24 @@ Public Class frmChat
         End If
     End Sub
 
+    Private Sub GetIPAddress()
+        Dim HostName As String
+        Dim IPAddresses As String = ""
+
+        HostName = System.Net.Dns.GetHostName()
+
+        For Each IP In System.Net.Dns.GetHostEntry(HostName).AddressList
+            IPAddresses += IP.ToString() + vbCrLf
+        Next
+        MessageBox.Show("Host Name: " + HostName + "; IP Addresses: " + vbCrLf + IPAddresses)
+    End Sub
+
+    Private Sub GetMyIpButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GetMyIpButton.Click
+        GetIPAddress()
+    End Sub
+
+    Private Sub ClearButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClearButton.Click
+        ServerTextBox.Clear()
+        ClientTextBox.Clear()
+    End Sub
 End Class
